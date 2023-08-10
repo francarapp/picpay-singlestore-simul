8df3e49dd02664446a9b49c15bd514364f9d3b62
Frank Cara <frank.cara@picpay.com>
2023-08-10 08:11:48 -0400

    JSON

--- pkg/simul/event_distribution.go
+++ pkg/simul/event_distribution.go
@@ -17,6 +17,12 @@ var dist = distuv.Normal{
 	Src:   grand.NewSource(uint64(time.Now().UnixNano())),
 }
 
+var distJsonKeys = distuv.Normal{
+	Mu:    20, // Mean of the normal distribution
+	Sigma: 5,  // Standard deviation of the normal distribution
+	Src:   grand.NewSource(uint64(time.Now().UnixNano())),
+}
+
 func genEventName() string {
 	index := uint64(dist.Rand())
 	if index > 1000 {
@@ -26,7 +32,10 @@ func genEventName() string {
 }
 
 func genPayload(eventName string) string {
-	q := rand.Intn(100)
+	q := int(distJsonKeys.Rand())
+	if q == 0 {
+		q = 1
+	}
 	json := []string{
 		fmt.Sprintf("\"value\": %f", math.Trunc(math.Abs(rand.NormFloat64()*100))),
 	}
@@ -44,7 +53,7 @@ func genLabels(eventName string) string {
 	fGenLabel := func() []string {
 		lbs := []string{}
 		rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-		qtd := rnd.Intn(4)
+		qtd := rnd.Intn(10)
 		for i := 0; i < qtd; i++ {
 			rnd.Seed(time.Now().UnixNano())
 			lbs = append(lbs, fmt.Sprintf("bus%d", rnd.Intn(10)+1))
